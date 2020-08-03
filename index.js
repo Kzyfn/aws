b35d1c33159d3e95c2e767e78f2810d21557cc22
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-08-04 08:54:13 +0900

    a

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 var express = require('express');
-
-const passport = require('passport');
-const passportHttp = require('passport-http');
-
-passport.use(new passportHttp.BasicStrategy(
-  function (username, password, done) {
-    if (username === 'amazon' && password == 'candidate') {
-      return done(null, true);
-    } else {
-      return done(null, false);
-    }
-  }
-));
-
 const app = express();
+var auth = express.basicAuth('amazon', 'candidate');
 
 app.set("view engine", "ejs");
-app.get('/secret',
-  passport.authenticate('basic', { session: false, }),
-  (req, res) => {
-    res.render("index", { content: "Success" });
-  });
+
 
 app.listen(3000, (err, res) => {
   console.log('server is launched');
 });
-
 // HTTPリクエストを受け取る部分
 app.get('/', function (req, res) {
   res.send('AMAZON');
 });
 
+app.get('/private', auth, function (req, res) {
+  res.send('Private information');
+});
 
 // サーバーを起動する部分
 var server = app.listen(3000, function () {
